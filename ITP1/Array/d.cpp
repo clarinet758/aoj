a61bb9c54f08377a74513dc194cccfc3633a2a70
clarinet758 <clarinet758@gmail.com>
2015-05-05 19:35:15 +0900

    done

--- ITP1/Array/d.cpp
+++ ITP1/Array/d.cpp
@@ -16,24 +16,23 @@ int main(){
     double pai=3.141592653589;
     int n,m,t;
     scanf("%d %d",&n,&m);
-    int a[n-1][m-1], b[m-1], ans[n-1];
-    for(int i=0;i<n-1;i++){
-        for(int j=0;j<m-1;j++){
+    int a[n][m], b[m], ans[n];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
             scanf("%d",&a[i][j]);
-        }
+       }
     }
-    for(int i=0;i<m-1;i++){
+    for(int i=0;i<m;i++){
         scanf("%d",&b[i]);
     }
-    for(int i=0;i<n-1;i++){
+    for(int i=0;i<n;i++){
         t=0;
-        for(int j=0;j<m-1;j++){
-            printf("%d %d\n",a[i][j],b[j]);
-            t+=a[i][j]*b[j];
+        for(int j=0;j<m;j++){
+            t+=(a[i][j]*b[j]);
         }
         ans[i]=t;
     }
-    for(int i=0;i<n-1;i++){
+    for(int i=0;i<n;i++){
         printf("%d\n",ans[i]);
     }
     return 0;
