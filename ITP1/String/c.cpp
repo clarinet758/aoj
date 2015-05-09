d9ed749b6b8a4fa9f9223978f059ed1d73a2922a
clarinet758 <clarinet758@gmail.com>
2015-05-10 04:01:46 +0900

    saigoppe

---
+++ ITP1/String/c.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+/**
+ * vector<int>ar(3);
+ * for(auto&e:ar){
+ *     scanf("%d",&e);
+ * }
+ * sort(ar.begin(),ar.end())
+ * int sum=accumulate(ar.begin(),ar.end(),0);
+ ***/
+
+int main(){
+    double pai=3.141592653589;
+    int n,taro=0,hanako=0;
+    char t[100],h[100];
+    string tarostr,hanakostr;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        scanf("%s %s",&t,&h);
+        tarostr=t;
+        hanakostr=h;
+        if(tarostr==hanakostr){
+            taro++;
+            hanako++;
+        }else if(tarostr<hanakostr){
+            hanako+=3;
+        }else{
+            taro+=3;
+        }
+    }
+    printf("%d %d\n",taro,hanako);
+    return 0;
+}
