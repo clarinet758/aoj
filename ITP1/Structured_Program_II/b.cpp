319241667168c5d51b94026a8554eb71b81e948b
clarinet758 <clarinet758@gmail.com>
2015-05-20 02:35:30 +0900

    bekkai

--- ITP1/Structured_Program_II/b.cpp
+++ ITP1/Structured_Program_II/b.cpp
@@ -21,10 +21,8 @@ int main(){
         if(n==0) break;
         for(int i=1;i<n-1;i++){
             for(int j=i+1;j<n;j++){
-                for(int k=j+1;k<=n;k++){
-                    if(i+j+k==x){
-                        ans++;
-                    }
+                if(n>=x-i-j && j<x-i-j){
+                    ans++;
                 }
             }
         }
