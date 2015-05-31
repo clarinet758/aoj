98f1c880fd102bedc62af8660c05715b03cf825c
clarinet758 <clarinet758@gmail.com>
2015-05-31 23:49:58 +0900

    pra

--- ALDS1/2_Sort_I/a.cpp
+++ ALDS1/2_Sort_I/a.cpp
@@ -14,23 +14,29 @@ using namespace std;
 
 int main(){
     double pai=3.141592653589;
-    int n,memo=1000000001,r[200000],ans=-1000000001;
+    int n,ans=0,chk,tmp;
     scanf("%d",&n);
-    for(int i=0;i<n;i++){
-        scanf("%d",&r[i]);
+    vector<int> ar(n);
+    for(auto&e:ar){
+        scanf("%d",&e);
     }
-    for(int i=0;i<n-1;i++){
-        if(memo<r[i]){
-            continue;
-        }else{
-            memo=r[i];
-        }
-        for(int j=i+1;j<n;j++){
-            if(r[j]-r[i]>ans){
-                ans=r[j]-r[i];
+    for(;;){
+        chk=0;
+        for(int i=1;i<n;i++){
+            if(ar[i-1]>ar[i]){
+                tmp=ar[i];
+                ar[i]=ar[i-1];
+                ar[i-1]=tmp;
+                ans++;
+                chk=1;
             }
         }
+        if(chk==0) break;
     }
+    for(int i=0;i<n-1;i++){
+        printf("%d ",ar[i]);
+    }
+    printf("%d\n",ar[n-1]);
     printf("%d\n",ans);
     return 0;
 }
