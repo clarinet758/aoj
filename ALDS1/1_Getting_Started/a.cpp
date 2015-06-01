2632f114969c23e803a3ae9527a8ecff941ae005
clarinet758 <clarinet758@gmail.com>
2015-06-01 13:37:26 +0900

    oooops

--- ALDS1/1_Getting_Started/a.cpp
+++ ALDS1/1_Getting_Started/a.cpp
@@ -13,15 +13,17 @@ using namespace std;
  ***/
 
 int main(){
-    double pai=3.141592653589;
-    char w;
     int n;
-    int a[n];
     scanf("%d",&n);
+    int a[n];
     for(int i=0;i<n;i++){
         scanf("%d",&a[i]);
     }
-    for(int i=0;i<n;i++){
+    for(int i=1;i<=n;i++){
+        for(int p=0;p<n-1;p++){
+            printf("%d ",a[p]);
+        }
+        printf("%d\n",a[n-1]);
         int v=a[i];
         int j=i-1;
         while(j>=0 && a[j]>v){
@@ -29,10 +31,6 @@ int main(){
             j--;
         }
         a[j+1]=v;
-        for(int j=0;j<n-1;j++){
-            printf("%d ",a[j]);
-        }
-        printf("%d\n",a[-1]);
     }
     return 0;
 }
