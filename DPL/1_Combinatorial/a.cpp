a045bbce0fab263f54ebd1f31951c09932f24490
clarinet758 <clarinet758@gmail.com>
2015-09-12 02:32:07 +0900

    vector

--- DPL/1_Combinatorial/a.cpp
+++ DPL/1_Combinatorial/a.cpp
@@ -6,32 +6,24 @@
 #include<algorithm>
 using namespace std;
 
-/**
- * vector<int>ar(3);
- * for(auto&e:ar){
- *     scanf("%d",&e);
- * }
- * sort(ar.begin(),ar.end())
- * int sum=accumulate(ar.begin(),ar.end(),0);
- ***/
-
 int main(){
-    double pai=3.141592653589;
     int n,m;
     scanf("%d %d",&n,&m);
-    vector<int> ar(m);
-    for(auto&e:ar){
+    vector<int> coin(m);
+    for(auto&e:coin){
         scanf("%d",&e);
     }
-    int INF=1<<28, MAX_N=50010;
-    int dp[MAX_N];
-    fill(dp, dp+MAX_N, INF);
-    dp[n]=0;
+    int INF=1<<18, MAX_N=50001;
+    vector<int> dp_coin(MAX_N);
+    fill(dp_coin.begin(), dp_coin.end(), INF);
+    dp_coin[n]=0;
     for(int i=n;i>0;i--){
         for(int j=0;j<m;j++){
-            dp[i-ar[j]]=min(dp[i]+1,dp[i-ar[j]]);
+            if(i>=coin[j]){
+                dp_coin[i-coin[j]]=min(dp_coin[i]+1,dp_coin[i-coin[j]]);
+            }
         }
     }
-    printf("%d\n",dp[0]);
+    printf("%d\n",dp_coin[0]);
     return 0;
 }
