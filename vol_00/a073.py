4ea6bf2bda685edf62dff4306f566e42e4d8e177
nyon <clarinet758@gmail.com>
2015-12-24 01:30:40 +0900

    bb

---
+++ vol_00/a073.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+import time
+import sys
+import io
+import re
+import math
+import itertools
+import collections
+#sys.stdin=file('input.txt')
+#sys.stdout=file('output.txt','w')
+#10**9+7
+mod=1000000007
+#mod=1777777777
+pi=3.141592653589
+xy=[(1,0),(-1,0),(0,1),(0,-1)]
+bs=[(-1,-1),(-1,1),(1,1),(1,-1)]
+def gcd(a,b): return a if b==0 else gcd(b,a%b)
+def lcm(a,b): return a*b/gcd(a,b)
+def choco(xa,ya,xb,yb,xc,yc,xd,yd): return 1 if abs((yb-ya)*(yd-yc)+(xb-xa)*(xd-xc))<1.e-10 else 0
+while 1:
+    x=int(raw_input())
+    h=int(raw_input())
+    if x==h==0:
+        break
+    print x*((4*h*h+x*x)**0.5)+x*x
+n,k=map(int,raw_input().split())
+l=map(int,raw_input().split())
+ans=chk=0
+#end = time.clock()
+#print end - start
