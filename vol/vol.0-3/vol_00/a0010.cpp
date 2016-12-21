346ee8c0fd5ecef90546a2863ffeeaae604bcbfc
clarinet758 <clarinet758@gmail.com>
2016-12-22 03:44:27 +0900

    符号間違い探し。。

--- vol/vol.0-3/vol_00/a0010.cpp
+++ vol/vol.0-3/vol_00/a0010.cpp
@@ -3,34 +3,93 @@
 #include<list>
 #include<stack>
 #include<queue>
-#include<cmath>
 #include<algorithm>
+#include<complex>
+#include<cmath>
 using namespace std;
 
-void sol(){
-    double x1,y1,x2,y2,x3,y3;
-    scanf("%lf %lf %lf %lf %lf %lf",&x1,&y1,&x2,&y2,&x3,&y3);
-    double a=sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
-    double b=sqrt((x1-x3)*(x1-x3)+(y1-y3)*(y1-y3));
-    double c=sqrt((x2-x3)*(x2-x3)+(y2-y3)*(y2-y3));
-    double s=(a+b+c)/2;
-    double ss=sqrt(s*(s-a)*(s-b)*(s-c));
-    double sina=2*ss/b/c;
-    double r=a/sina/2;
-    a*=a;
-    b*=b;
-    c*=c;
-    double px=(a*(b+c-a)*x3+b*(a+c-b)*x2+c*(a+b-c)*x1)/16/(ss*ss);
-    double py=(a*(b+c-a)*y3+b*(a+c-b)*y2+c*(a+b-c)*y1)/16/(ss*ss);
-    printf("%.3lf %.3lf %.3lf\n",px,py,r);
+#define PI 3.1415926535898
 
-}
+//点座標を型とする
+typedef complex<double> P;
+
+
+//許容誤差
+#define EPS (1e-10)
+//X Y座標
+#define X real()
+#define Y imag()
+
+class Triangle {
+    private:
+    //三角形の3点の座標
+    P a,b,c;
+    //三角形の辺の長さ
+    double edgeA, edgeB, edgeC;
+    //三角形の角の大きさ
+    double angleA, angleB, angleC;
+    //余弦定理から3つの角度を求める
+    double LawOfCosines(double a, double b, double c) {
+        return acos((b*b+c*c-a*a)/(2.0*b*c));
+    }
+    //2つのdouble型が等しいか
+    bool equal(double a,double b) {
+        return (abs(a-b)<EPS)?true:false;
+    }
+
+    public:
+    //コンストラクタ
+    Triangle(P p1, P p2, P p3) {
+        a=p1;
+        b=p2;
+        c=p3;
+        edgeB=abs(c-a);
+        edgeA=abs(b-c);
+        edgeC=abs(a-b);
+        angleA=LawOfCosines(edgeA, edgeB, edgeC);
+        angleB=LawOfCosines(edgeB, edgeC, edgeA);
+        angleC=LawOfCosines(edgeC, edgeA, edgeB);
+    }
+
+    double circumscribedCircleRadius() {//外接円の半径
+        return (edgeA/sin(angleA)/2.0);
+    }
+
+    double circumscribedCircleX() {//外接円の中心X座標
+        double A=sin(2.0*angleA);
+        double B=sin(2.0*angleB);
+        double C=sin(2.0*angleC);
+        return ((a.X * A + b.X * B + c.X * C)/(A+B+C));
+    }
+
+    double circumscribedCircleY() {//外接円の中心Y座標
+        double A=sin(2.0*angleA);
+        double B=sin(2.0*angleB);
+        double C=sin(2.0*angleC);
+        return ((a.Y * A + b.Y * B + c.Y * C)/(A+B+C));
+    }
+};
+
 
 int main(){
+    int mod=1000000007;
     int n;
+    double x1,y1,x2,y2,x3,y3;
+    double r,xp,yp;
     scanf("%d",&n);
-    for(int i=0;i<n;i++){
-        sol();
+    for (int i=0;i<n;i++) {
+        scanf("%lf %lf %lf %lf %lf %lf",&x1,&y1,&x2,&y2,&x3,&y3);
+        P a(x1,y1);
+        P b(x2,y2);
+        P c(x3,y3);
+        Triangle Tr(a,b,c);
+        xp=Tr.circumscribedCircleX();
+        yp=Tr.circumscribedCircleY();
+        r =Tr.circumscribedCircleRadius();
+
+        //printf("%.10f %.10f %.10f\n",xp,yp,r);
+        printf("%.3f %.3f %.3f\n",xp,yp,r);
     }
+
     return 0;
 }
