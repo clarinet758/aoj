fd1de06194dd3a3c390f10325d7df5741581f025
clarinet758 <clarinet758@gmail.com>
2016-12-30 12:55:09 +0900

    short

--- vol/vol.0-3/vol_00/a0048.cpp
+++ vol/vol.0-3/vol_00/a0048.cpp
@@ -9,14 +9,6 @@
 #include<complex>
 using namespace std;
 
-typedef complex<double> P;
-
-#define X real()
-#define Y imag()
-typedef int kz;
-
-kz gcd(kz a,kz b) { if(b==0) return a; else gcd(b,a%b); }
-
 int main(){
     double s;
     while (scanf("%lf",&s)!=EOF){
@@ -43,31 +35,6 @@ int main(){
         } else {
             printf("heavy\n");
         }
-        /** WA
-        if ((s-91.0)>1e-10) {
-            printf("heavy\n");
-        } else if ((s-81.0)>1e-10) {
-            printf("light heavy\n");
-        } else if ((s-75.0)>1e-10) {
-            printf("middle\n");
-        } else if ((s-69.0)>1e-10) {
-            printf("light middle\n");
-        } else if ((s-64.0)>1e-10) {
-            printf("welter\n");
-        } else if ((s-60.0)>1e-10) {
-            printf("light welter\n");
-        } else if ((s-57.0)>1e-10) {
-            printf("light\n");
-        } else if ((s-54.0)>1e-10) {
-            printf("feather\n");
-        } else if ((s-51.0)>1e-10) {
-            printf("bantam\n");
-        } else if ((s-48.0)>1e-10) {
-            printf("fly\n");
-        } else {
-            printf("light fly\n");
-        }
-        **/
     }
     return 0;
 }
--- vol/vol.0-3/vol_00/a0049.cpp
+++ vol/vol.0-3/vol_00/a0049.cpp
@@ -9,14 +9,6 @@
 #include<complex>
 using namespace std;
 
-typedef complex<double> P;
-
-#define X real()
-#define Y imag()
-typedef int kz;
-
-kz gcd(kz a,kz b) { if(b==0) return a; else gcd(b,a%b); }
-
 int main(){
     int n;
     char s[2];
