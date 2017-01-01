b3cd1d23ff59419d60fc238e8aee9f1b1edba6d5
clarinet758 <clarinet758@gmail.com>
2017-01-01 21:27:07 +0900

    scan

--- vol/vol.0-3/vol_00/a0063.cpp
+++ vol/vol.0-3/vol_00/a0063.cpp
@@ -7,20 +7,21 @@
 #include<complex>
 using namespace std;
 
-int main(){
-    char str[102];
-    int len,f,ans=0;
-    while (fgets(str,102,stdin)!=NULL) {
-        len=strlen(str)-1;
-        f=1;
-        for (int i=0;i<len/2;++i) {
-            if(str[i]!=str[len-i-1]){
+int main() {
+    char s[111];
+    int ans=0;
+    while(scanf("%s",s)!=EOF) {
+        int tmp=strlen(s);
+        int f=1;
+        for (int i=0;i<tmp/2;++i) {
+            if (s[i]!=s[tmp-(i+1)]) {
                 f=0;
                 break;
             }
         }
         if (f) ans++;
     }
+
     printf("%d\n",ans);
     return 0;
 }
