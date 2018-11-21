d810e7a89dc4bd2e255d3dac1a2ccbc6211b69e3
clarinet758 <clarinet758@gmail.com>
2018-11-22 02:51:32 +0900

    em

--- Lessons/ITP1/01_Getting_Started/a.cpp
+++ Lessons/ITP1/01_Getting_Started/a.cpp
@@ -1,19 +1,7 @@
 #include<bits/stdc++.h>
-#include<vector>
-#include<algorithm>
 using namespace std;
 
-/**
- * vector<int>ar(3);
- * for(auto&e:ar){
- *     scanf("%d",&e);
- * }
- * sort(ar.begin(),ar.end())
- * int sum=accumulate(ar.begin(),ar.end(),0);
- ***/
-
 int main(){
-    double pai=3.141592653589;
     printf("Hello World\n");
     return 0;
 }
