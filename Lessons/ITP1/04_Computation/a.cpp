33fe53d6c6bdfc6bd30ed5b5f1c21c8221c96671
clarinet758 <clarinet758@gmail.com>
2018-12-05 02:53:15 +0900

    neru

--- Lessons/ITP1/04_Computation/a.cpp
+++ Lessons/ITP1/04_Computation/a.cpp
@@ -1,23 +1,8 @@
 #include<bits/stdc++.h>
-#include<vector>
-#include<algorithm>
 using namespace std;
-
-/**
- * vector<int>ar(3);
- * for(auto&e:ar){
- *     scanf("%d",&e);
- * }
- * sort(ar.begin(),ar.end())
- * int sum=accumulate(ar.begin(),ar.end(),0);
- ***/
-
 int main(){
-    double pai=3.141592653589,da,db;
     int a,b;
     scanf("%d %d",&a,&b);
-    da=static_cast<double>(a);
-    db=static_cast<double>(b);
-    printf("%d %d %.8lf\n",a/b,a%b,da/db);
+    printf("%d %d %.8lf\n",a/b,a%b,a/(1.0*b));
     return 0;
 }
