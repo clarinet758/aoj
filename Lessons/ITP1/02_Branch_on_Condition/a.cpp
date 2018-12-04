c2e88d79dc722bfa222a392015b07dcfa6ccf6c8
clarinet758 <clarinet758@gmail.com>
2018-12-05 02:00:25 +0900

    &忘れ

--- Lessons/ITP1/02_Branch_on_Condition/a.cpp
+++ Lessons/ITP1/02_Branch_on_Condition/a.cpp
@@ -1,23 +1,9 @@
 #include<bits/stdc++.h>
-#include<vector>
-#include<algorithm>
 using namespace std;
-
-/**
- * vector<int>ar(3);
- * for(auto&e:ar){
- *     scanf("%d",&e);
- * }
- * sort(ar.begin(),ar.end())
- * int sum=accumulate(ar.begin(),ar.end(),0);
- ***/
-
 int main(){
-    double pai=3.141592653589;
     int a,b;
     scanf("%d %d",&a,&b);
-    if (a>b) printf("a > b\n");
-    else if (a<b) printf("a < b\n");
+    if (a!=b) printf("%s\n",a>b?"a > b":"a < b");
     else printf("a == b\n");
     return 0;
 }
