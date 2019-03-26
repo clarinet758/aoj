7ea211537ba8b5a06879dc9d5f360203cd3ae219
clarinet758 <clarinet758@gmail.com>
2019-03-27 00:44:11 +0900

    hage

--- Lessons/ITP1/05_Structured_Program_I/d.cpp
+++ Lessons/ITP1/05_Structured_Program_I/d.cpp
@@ -1,23 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int n;
-    scanf("%d",&n);
-    for (int i=3;i<=n;i++) {
-        int x=i;
-        if (i%3==0 || i%10==3) {
+void sol(int x,int n) {
+    int i=1;
+    x=i;
+    for (;;) {
+        x=i;
+        if (x%3==0 || x%10==3) {
             printf(" %d",i);
         } else {
-            while (x>0) {
+            for(;;) {
+                x/=10;
                 if (x%10==3) {
                     printf(" %d",i);
                     break;
                 }
-                x/=10;
+                if (x==0) break;
             }
         }
+        i++;
+        if (i>n) break;
     }
+}
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    sol(n,n);
     printf("\n");
     return 0;
 }
