5699b7e8b225b3d64a8c0c840593adfa13ac705a
clarinet758 <clarinet758@gmail.com>
2019-03-29 23:44:31 +0900

    toilet

--- Lessons/ITP1/06_Array/b.cpp
+++ Lessons/ITP1/06_Array/b.cpp
@@ -1,49 +1,26 @@
 #include<bits/stdc++.h>
-#include<vector>
-#include<algorithm>
 using namespace std;
 
-/**
- * vector<int>ar(3);
- * for(auto&e:ar){
- *     scanf("%d",&e);
- * }
- * sort(ar.begin(),ar.end())
- * int sum=accumulate(ar.begin(),ar.end(),0);
- ***/
-
 int main(){
-    double pai=3.141592653589;
-    int n,p;
-    char s;
-    int card[4][13];
+    int n,k;
+    char s[5];
+    bool d[4][13];
+    for (int i=0;i<52;i++) d[i/13][i%13]=0;
     scanf("%d",&n);
-    for(int i=0;i<4;i++){
-        for(int j=0;j<13;j++){
-            card[i][j]=0;
-        }
+    for (int i=0;i<n;i++) {
+        scanf("%s %d",&s,&k);
+//        printf("%c %d\n",s[0],k);
+        if (s[0]=='S') d[0][k-1]=1;
+        if (s[0]=='H') d[1][k-1]=1;
+        if (s[0]=='C') d[2][k-1]=1;
+        if (s[0]=='D') d[3][k-1]=1;
     }
-    for(int i=0;i<n;i++){
-        scanf("%s %d",&s,&p);
-        if(s=='S'){
-            card[0][p-1]=1;
-        }else if(s=='H'){
-            card[1][p-1]=1;
-        }else if(s=='C'){
-            card[2][p-1]=1;
-        }else if(s=='D'){
-            card[3][p-1]=1;
-        }
-    }
-
-    for(int i=0;i<4;i++){
-        for(int j=0;j<13;j++){
-            if(card[i][j]==0){
-                if(i==0) printf("S %d\n",j+1);
-                if(i==1) printf("H %d\n",j+1);
-                if(i==2) printf("C %d\n",j+1);
-                if(i==3) printf("D %d\n",j+1);
-            }
+    for (int i=0;i<52;i++) {
+        if (d[i/13][i%13]==0) {
+            if (i/13==0) printf("S %d\n",i%13+1);
+            if (i/13==1) printf("H %d\n",i%13+1);
+            if (i/13==2) printf("C %d\n",i%13+1);
+            if (i/13==3) printf("D %d\n",i%13+1);
         }
     }
     return 0;
--- Lessons/ITP1/06_Array/c.cpp
+++ Lessons/ITP1/06_Array/c.cpp
@@ -1,44 +1,24 @@
 #include<bits/stdc++.h>
-#include<vector>
-#include<algorithm>
 using namespace std;
 
-/**
- * vector<int>ar(3);
- * for(auto&e:ar){
- *     scanf("%d",&e);
- * }
- * sort(ar.begin(),ar.end())
- * int sum=accumulate(ar.begin(),ar.end(),0);
- ***/
-
 int main(){
-    double pai=3.141592653589;
+    int mod=1000000007;
     int n,b,f,r,v;
-    int room[4][3][10];
+    int d[4][3][10];
+    for (int i=0;i<4;i++) for (int j=0;j<3;j++) for (int k=0;k<10;k++) d[i][j][k]=0;
     scanf("%d",&n);
-    for(int i=0;i<4;i++){
-        for(int j=0;j<3;j++){
-            for(int k=0;k<10;k++){
-                room[i][j][k]=0;
-            }
-        }
-    }
-    for(int i=0;i<n;i++){
+    for (int i=0;i<n;i++) {
         scanf("%d %d %d %d",&b,&f,&r,&v);
-        room[b-1][f-1][r-1]+=v;
+        d[b-1][f-1][r-1]+=v;
     }
-    for(int i=0;i<4;i++){
-        for(int j=0;j<3;j++){
-            for(int k=0;k<10;k++){
-                printf(" %d",room[i][j][k]);
-            }
-            if(j!=2 || (j==2 && i==3)){
-                printf("\n");
-            }else if(i!=3){
-                printf("\n####################\n");
+    for (int i=0;i<4;i++) {
+        for (int j=0;j<3;j++) {
+            for (int k=0;k<10;k++) {
+                printf(" %d",d[i][j][k]);
+                if (k==9) printf("\n");
             }
         }
+        if (i<3) printf("####################\n");
     }
     return 0;
 }
