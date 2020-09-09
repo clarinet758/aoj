bfb5722c9bc12a2843129b4bcf9e84ddfabad7ed
nyon <clarinet758@gmail.com>
2020-09-10 01:49:40 +0900

    らせん本

--- Lessons/ALDS1/04_Search/b.cpp
+++ Lessons/ALDS1/04_Search/b.cpp
@@ -4,17 +4,27 @@ using namespace std;
 #define rep(i,n)  for(int i=0;i<n;++i)
 #define sc1(a)  scanf("%d",&a)
 
+int n,s[100005];
+
+bool chk(int k){
+    int l=0,r=n,x;
+    while (l<r) {
+        x=(l+r)/2;
+        if (k==s[x]) return 1;
+        if (k>s[x]) l=x+1;
+        else r=x;
+    }
+    return 0;
+}
+
 int main(){
-    int n,q,ans=0;
+    int t,q,ans=0;
     sc1(n);
-    //int s[n];
-    vector <int> s(n);
     rep(i,n) sc1(s[i]);
     sc1(q);
-    int t[n];
     rep(i,q) {
-        sc1(t[i]);
-        ans+=binary_search(s.begin(),s.end(),t[i]);
+        sc1(t);
+        ans+=chk(t);
     }
     printf("%d\n",ans);
     return 0;
