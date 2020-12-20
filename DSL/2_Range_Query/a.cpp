75336116e0a1e3235690fdeded10647fb08ccf1c
nyon <clarinet758@gmail.com>
2020-12-20 21:44:44 +0900

    再帰

--- DSL/2_Range_Query/a.cpp
+++ DSL/2_Range_Query/a.cpp
@@ -6,52 +6,36 @@ using namespace std;
 #define sc2(a,b)  scanf("%d %d",&a,&b)
 #define sc3(a,b,c)  scanf("%d %d %d",&a,&b,&c)
 #define sl3(a,b,c)  scanf("%lld %lld %lld",&a,&b,&c)
-#define SEG_LEN (1ll<<18)
 
-long long seg[SEG_LEN * 2ll];
-long long p = 1ll<<31;
+long long inf=(1ll<<31)-1;
+long long seg[1<<20];
 
-void add (long long ind,long long v) {
-    ind += SEG_LEN*1ll;
+void val(long long ind,long long v) {
+    ind += 1<<19;
     seg[ind]=v;
-    for(;;){
-        ind/=2ll;
-        if (ind==0ll) break;
-        seg[ind]=min(seg[ind*2ll],seg[ind*2ll+1ll]);
-    }
+    while ((ind/=2)>0) seg[ind]=min(seg[ind*2],seg[ind*2+1]);
 }
 
-long long  get(long long l, long long r) {
-    l+=SEG_LEN*1ll;
-    r+=SEG_LEN*1ll;
-    long long ans=p;
-    for(;;){
-        if (l>=r) break;
-        if(l%2ll==1ll){
-            ans=min(ans,seg[l]);
-            l++;
-        }
-        l/=2ll;
-        if(r%2ll==1ll){
-            ans=min(ans,seg[r-1ll]);
-            r--;
-        }
-        r/=2ll;
-    }
-    return ans;
+long long getm(long long ql,long long qr,long long sl=0ll,long long sr=1<<19,long long pos=1ll){
+    if (qr<=sl || sr<=ql) return inf;
+    if (ql<=sl && sr<=qr) return seg[pos];
+    long long sm=(sl+sr)/2;
+    long long lmin =getm(ql,qr,sl,sm,pos*2);
+    long long rmin =getm(ql,qr,sm,sr,pos*2+1);
+    return min(lmin,rmin);
 }
 
 int main(){
-    p-=1;
-    //cout<< p << endl;
+    //cout<<inf  << endl;
     int n,q;
     sc2(n,q);
-    for(long long i=0;i<SEG_LEN*2ll;i++) seg[i]=p;
+    rep(i,n) val(i,inf);
     rep(i,q){
         long long a,b,c;
         sl3(a,b,c);
-        if(a==0ll) add(b,c);
-        if(a==1ll) printf("%lld\n",get(b,c+1ll));
+        if (a==0) val(b,c);
+        else printf("%lld\n",getm(b,c+1));
     }
+
     return 0;
 }
